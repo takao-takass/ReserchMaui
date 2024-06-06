66f5e3f9f8938e9459193f067e9b579ddce7d968
Takahiro Tada <t.tada@kms3.com>
2024-06-06 13:52:05 +0900

    project initialized.

---
+++ ReserchMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReserchMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
